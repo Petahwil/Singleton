7dbb44c38d965c74b5d39886b53fa79c30eca909
Petahwil <petahwil@Petahwils-MacBook-Pro.local>
2013-10-22 16:07:21 -0400

    Initial Commit

---
+++ Singleton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Singleton
+//
+//  Created by Petahwil on 10/22/13.
+//  Copyright (c) 2013 Petahwil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Singleton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Singleton
+//
+//  Created by Petahwil on 10/22/13.
+//  Copyright (c) 2013 Petahwil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
